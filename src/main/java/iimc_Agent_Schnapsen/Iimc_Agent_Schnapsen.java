67fdf65e8f9483d98595251522963d7a6864cef4
binaph <philipp_bina@hotmail.com>
2026-03-24 15:23:49 +0100

    added an imperfect information monte carlo agent;

---
+++ src/main/java/iimc_Agent_Schnapsen/Iimc_Agent_Schnapsen.java
@@ -0,0 +1,387 @@
+package iimc_Agent_Schnapsen;
+
+import at.ac.tuwien.ifs.sge.agent.AbstractGameAgent;
+import at.ac.tuwien.ifs.sge.agent.GameAgent;
+import at.ac.tuwien.ifs.sge.engine.Logger;
+import at.ac.tuwien.ifs.sge.util.Util;
+import game.Schnapsen;
+import game.action.SchnapsenAction;
+import game.board.PlayingCard;
+import game.board.SchnapsenBoard;
+
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class Iimc_Agent_Schnapsen extends AbstractGameAgent<Schnapsen, SchnapsenAction> implements GameAgent<Schnapsen, SchnapsenAction> {
+
+    //used for round end check
+    private double oldUtilityPlayer0;
+    private double oldUtilityPlayer1;
+
+    public Iimc_Agent_Schnapsen(Logger log) {
+        super(log);
+    }
+
+    @Override
+    public SchnapsenAction computeNextAction(Schnapsen schnapsen, long l, TimeUnit timeUnit) {
+
+        SchnapsenBoard board = schnapsen.getBoard();
+        Set<SchnapsenAction> availableActions = schnapsen.getPossibleActions();
+
+        // Single choice actions can be returned immediately
+        if (availableActions.size() == 1) {
+            return availableActions.iterator().next();
+        }
+
+        setTimers(l, timeUnit);
+
+        // Set the utility values for end of round check in simulations
+        oldUtilityPlayer0 = schnapsen.getUtilityValue(0);
+        oldUtilityPlayer1 = schnapsen.getUtilityValue(1);
+
+        List<PlayingCard> deckOfCards = generateFullDeck(board.getTrumpCard().getSuit());
+
+        // Setting starting values for Imperfect Information Monte Carlo
+        Map<SchnapsenAction, Double> value = new HashMap<>();
+        Map<SchnapsenAction, Integer> visits = new HashMap<>();
+        for (SchnapsenAction action : availableActions) {
+            value.put(action, 0.0);
+            visits.put(action, 0);
+        }
+
+        // track how often we sample
+        int worldsSampled = 0;
+
+        // let the algorithm sample till the time limit is reached
+        while (!shouldStopComputation()) {
+
+            // create a new perfect information board (sample world)
+            SchnapsenBoard generatedBoard = generateMissingInformation(board, deckOfCards);
+            Schnapsen sampleWorld = new Schnapsen(generatedBoard);
+
+            // every move should be tried till reaching a game over or round end state
+            for (SchnapsenAction m : availableActions) {
+                if (shouldStopComputation()) break;
+
+                // simulation or playout
+                double v = finishedGameValue(sampleWorld, m);
+
+                // track each actions value
+                value.put(m, value.get(m) + v);
+                visits.put(m, visits.get(m) + 1);
+            }
+
+            worldsSampled++;
+        }
+
+        log._debugf("IIMC completed %d sampled worlds", worldsSampled);
+
+        // select the best action based on their statistics
+        log.debug("--- Action Statistics ---");
+        SchnapsenAction bestAction = null;
+        double bestAverageScore = -1.0;
+
+        for (SchnapsenAction action : availableActions) {
+            int actionVisits = visits.get(action);
+            double score = value.get(action);
+            double avgScore = (actionVisits > 0) ? (score / actionVisits) : 0.0;
+
+            log._debugf("Action: %-20s | Playouts: %6d | Expected Value: %5.2f",
+                    action.toString(),
+                    actionVisits,
+                    avgScore);
+
+            if (avgScore > bestAverageScore) {
+                bestAverageScore = avgScore;
+                bestAction = action;
+            }
+        }
+
+        if (bestAction == null) {
+            return availableActions.iterator().next();
+        }
+
+        log._debugf("--> CHOSEN ACTION: %s", bestAction.toString());
+        return bestAction;
+    }
+
+    /**
+     * Represents one possible playout (till end of game or end of round) for the action and game passed in the method
+     * @param schnapsen the current game state
+     * @param schnapsenAction the action to be evaluated
+     * @return a double containing the score at the end of the simulation
+     */
+    private double finishedGameValue(Schnapsen schnapsen, SchnapsenAction schnapsenAction) {
+
+        Schnapsen playoutSchnapsen = (Schnapsen) schnapsen.doAction(schnapsenAction);
+
+        while (!shouldStopComputation() && !playoutSchnapsen.isGameOver() && !isRoundOver(playoutSchnapsen)) {
+
+            // we select a random action and apply it to the game
+            Set<SchnapsenAction> possibleActions = playoutSchnapsen.getPossibleActions();
+            SchnapsenAction playoutAction = Util.selectRandom(possibleActions);
+
+            playoutSchnapsen = (Schnapsen) playoutSchnapsen.doAction(playoutAction);
+        }
+
+        // return the simulations score
+        return simulationScore(playoutSchnapsen);
+    }
+
+    private double simulationScore(Schnapsen currentGame) {
+        SchnapsenBoard board = currentGame.getBoard();
+        if(currentGame.isGameOver()){
+            if(playerId == 0)
+            {
+                if(board.getPlayer0BummerlAmount() < board.getBummerlMax())
+                {
+                    return 1.0;
+                } else {
+                    return 0.0;
+                }
+            }
+            else {
+                if(board.getPlayer1BummerlAmount() < board.getBummerlMax())
+                {
+                    return 1.0;
+                } else {
+                    return 0.0;
+                }
+            }
+        } else if(!this.isRoundOver(currentGame))
+        {
+            double myUtilValue = currentGame.getUtilityValue(playerId) % 1.0;
+            double theirUtilValue = currentGame.getUtilityValue(1-playerId) % 1.0;
+            double utilValue = (myUtilValue - theirUtilValue + 1.0) / 2.0;
+            return utilValue;
+        } else {
+            if (playerId == 0) {
+                long newUtilValuePlayer0 = (long) currentGame.getUtilityValue(0);
+                if (newUtilValuePlayer0 > oldUtilityPlayer0) {
+                    return 1.0;
+                } else {
+                    return 0.0;
+                }
+            } else {
+                long newUtilValuePlayer1 = (long) currentGame.getUtilityValue(1);
+                if (newUtilValuePlayer1 > oldUtilityPlayer1) {
+                    return 1.0;
+                } else {
+                    return 0.0;
+                }
+            }
+        }
+    }
+
+    /**
+     * This method takes all available information of the given board and randomizes the unknown cards into the drafting pile and opposing players hand
+     * @param board the actual game board with hidden information
+     * @return a new deep copied board with the given information and randomized cards for all unknown cards in the pile and opposing players hand
+     */
+    private SchnapsenBoard generateMissingInformation(SchnapsenBoard board, List<PlayingCard> cachedDeck) {
+        List<PlayingCard> unknownCards = new ArrayList<>(cachedDeck);
+        //Getting the players hand and all the marriage cards declared by the other player
+        List<PlayingCard> playerCards = new ArrayList<>();
+        List<PlayingCard> marriageCards = new ArrayList<>();
+        List<PlayingCard> otherPlayersCards = new ArrayList<>();
+        if(playerId == 0)
+        {
+            playerCards = board.getPlayer0Cards();
+            marriageCards = board.getPlayer1Marriages();
+        } else {
+            playerCards = board.getPlayer1Cards();
+            marriageCards = board.getPlayer0Marriages();
+        }
+
+        //Getting the trumpCard and the leading card
+        PlayingCard trumpCard = board.getTrumpCard();
+        PlayingCard leadingCard = board.getLeadingCard();
+
+        //Checking if there was an exchange of trumps
+        PlayingCard oldTrumpCard = board.getOldTrumpCard();
+
+        //Getting already played cards
+        List<PlayingCard> trickCards = new ArrayList<>();
+        List<PlayingCard[]> player0Tricks = board.getPlayer0Tricks();
+        List<PlayingCard[]> player1Tricks = board.getPlayer1Tricks();
+
+        for(PlayingCard[] trick: player0Tricks){
+            trickCards.add(trick[0]);
+            trickCards.add(trick[1]);
+        }
+        for(PlayingCard[] trick: player1Tricks){
+            trickCards.add(trick[0]);
+            trickCards.add(trick[1]);
+        }
+
+        //Check if "exchanged" trump card has been played
+        //(if not, check if they are in the opposing players hand == not in my hand)
+        if(oldTrumpCard != null) {
+
+            boolean isLeading = (leadingCard != null && leadingCard.equals(oldTrumpCard));
+
+            if (!trickCards.contains(oldTrumpCard) && !isLeading) {
+                if (!playerCards.contains(oldTrumpCard)) {
+                    otherPlayersCards.add(oldTrumpCard);
+                }
+            }
+        }
+
+        //check if opposing player played all of their declared marriageCards
+        for(PlayingCard marriageCard: marriageCards){
+
+            boolean isLeading =  (leadingCard != null && leadingCard.equals(marriageCard));
+
+            if(!trickCards.contains(marriageCard) && !isLeading && !otherPlayersCards.contains(marriageCard)) {
+                otherPlayersCards.add(marriageCard);
+            }
+        }
+
+        //Reduce the number of unknown Cards by all findings
+
+        //Remove players cards
+
+        unknownCards.removeAll(playerCards);
+
+        //Remove trumpCard
+        unknownCards.remove(trumpCard);
+
+        //Remove leadingCard
+        if(leadingCard != null) {
+            unknownCards.remove(leadingCard);
+        }
+
+        //Remove all of otherPlayers cards
+        unknownCards.removeAll(otherPlayersCards);
+
+        //Remove already played cards
+        unknownCards.removeAll(trickCards);
+
+
+        //Fill up all the hidden card-slots with random available cards
+
+        //shuffle the unknown cards
+        Collections.shuffle(unknownCards);
+
+        int numberOfHandCards = playerCards.size();
+        int numberOfOtherPlayersHandCards = numberOfHandCards;
+        int cardsLeftInPile = board.playingCardsLeftInPile();
+
+        //other player has one Card less, because he led the trick;
+        if(leadingCard != null) {
+            numberOfOtherPlayersHandCards--;
+        }
+
+        //first fill up the playingCardPile
+        LinkedList<PlayingCard> playingCardPile = new LinkedList<>();
+
+
+        for (int i = 0; i < cardsLeftInPile-1; i++) {
+            playingCardPile.add(unknownCards.removeFirst());
+        }
+
+        // Add trumpCard as last if pile was not empty yet
+        if(cardsLeftInPile > 0) {
+            playingCardPile.addLast(trumpCard);
+        } else {
+            boolean inMyHand = playerCards.contains(trumpCard);
+            boolean inTricks = trickCards.contains(trumpCard);
+            boolean isLeading = (leadingCard != null && leadingCard.equals(trumpCard));
+            boolean alreadyAdded = otherPlayersCards.contains(trumpCard);
+
+            if(!inMyHand && !inTricks && !isLeading && !alreadyAdded) {
+                otherPlayersCards.add(trumpCard);
+            }
+        }
+
+        //the remaining cards should fit into the opponents hand
+        if((otherPlayersCards.size() + unknownCards.size()) != numberOfOtherPlayersHandCards) {
+            throw new IllegalStateException("Calculated wrong: Opponent has " +
+                    (otherPlayersCards.size() + unknownCards.size()) +
+                    " but should have " + numberOfOtherPlayersHandCards);
+        } else {
+            otherPlayersCards.addAll(unknownCards);
+        }
+
+        //create a board based on the findings and randomly assumed cards
+        SchnapsenBoard filledBoard;
+        if(playerId == 0) {
+            filledBoard = new SchnapsenBoard(board, playerCards, otherPlayersCards, playingCardPile);
+        } else {
+            filledBoard = new SchnapsenBoard(board, otherPlayersCards, playerCards, playingCardPile);
+        }
+
+        return filledBoard;
+    }
+
+    private List<PlayingCard> generateFullDeck(SchnapsenBoard.cardSuits trumpSuit) {
+        //creating and filling a list with of all not known cards
+        LinkedList<PlayingCard> unknownCards = new LinkedList<>();
+
+        unknownCards.add(new PlayingCard(SchnapsenBoard.cardSuits.SPADES, SchnapsenBoard.cardNames.JackOfSpades, 2));
+        //Adding possible marriages to the spades cards
+        PlayingCard queenSpades = new PlayingCard(SchnapsenBoard.cardSuits.SPADES, SchnapsenBoard.cardNames.QueenOfSpades, 3);
+        PlayingCard kingSpades = new PlayingCard(SchnapsenBoard.cardSuits.SPADES, SchnapsenBoard.cardNames.KingOfSpades, 4);
+        queenSpades.setPossibleMarriage(kingSpades);
+        kingSpades.setPossibleMarriage(queenSpades);
+        unknownCards.add(queenSpades);
+        unknownCards.add(kingSpades);
+        unknownCards.add(new PlayingCard(SchnapsenBoard.cardSuits.SPADES, SchnapsenBoard.cardNames.TenOfSpades, 10));
+        unknownCards.add(new PlayingCard(SchnapsenBoard.cardSuits.SPADES, SchnapsenBoard.cardNames.AceOfSpades, 11));
+
+        unknownCards.add(new PlayingCard(SchnapsenBoard.cardSuits.HEARTS, SchnapsenBoard.cardNames.JackOfHearts, 2));
+        //Adding possible marriages to the hearts cards
+        PlayingCard queenHearts = new PlayingCard(SchnapsenBoard.cardSuits.HEARTS, SchnapsenBoard.cardNames.QueenOfHearts, 3);
+        PlayingCard kingHearts = new PlayingCard(SchnapsenBoard.cardSuits.HEARTS, SchnapsenBoard.cardNames.KingOfHearts, 4);
+        queenHearts.setPossibleMarriage(kingHearts);
+        kingHearts.setPossibleMarriage(queenHearts);
+        unknownCards.add(queenHearts);
+        unknownCards.add(kingHearts);
+        unknownCards.add(new PlayingCard(SchnapsenBoard.cardSuits.HEARTS, SchnapsenBoard.cardNames.TenOfHearts, 10));
+        unknownCards.add(new PlayingCard(SchnapsenBoard.cardSuits.HEARTS, SchnapsenBoard.cardNames.AceOfHearts, 11));
+
+        unknownCards.add(new PlayingCard(SchnapsenBoard.cardSuits.DIAMONDS, SchnapsenBoard.cardNames.JackOfDiamonds, 2));
+        //Adding possible marriages to the hearts cards
+        PlayingCard queenDiamonds = new PlayingCard(SchnapsenBoard.cardSuits.DIAMONDS, SchnapsenBoard.cardNames.QueenOfDiamonds, 3);
+        PlayingCard kingDiamonds = new PlayingCard(SchnapsenBoard.cardSuits.DIAMONDS, SchnapsenBoard.cardNames.KingOfDiamonds, 4);
+        queenDiamonds.setPossibleMarriage(kingDiamonds);
+        kingDiamonds.setPossibleMarriage(queenDiamonds);
+        unknownCards.add(queenDiamonds);
+        unknownCards.add(kingDiamonds);
+        unknownCards.add(new PlayingCard(SchnapsenBoard.cardSuits.DIAMONDS, SchnapsenBoard.cardNames.TenOfDiamonds, 10));
+        unknownCards.add(new PlayingCard(SchnapsenBoard.cardSuits.DIAMONDS, SchnapsenBoard.cardNames.AceOfDiamonds, 11));
+
+        unknownCards.add(new PlayingCard(SchnapsenBoard.cardSuits.CLUBS, SchnapsenBoard.cardNames.JackOfClubs, 2));
+        //Adding possible marriages to the hearts cards
+        PlayingCard queenClubs = new PlayingCard(SchnapsenBoard.cardSuits.CLUBS, SchnapsenBoard.cardNames.QueenOfClubs, 3);
+        PlayingCard kingClubs = new PlayingCard(SchnapsenBoard.cardSuits.CLUBS, SchnapsenBoard.cardNames.KingOfClubs, 4);
+        queenClubs.setPossibleMarriage(kingClubs);
+        kingClubs.setPossibleMarriage(queenClubs);
+        unknownCards.add(queenClubs);
+        unknownCards.add(kingClubs);
+        unknownCards.add(new PlayingCard(SchnapsenBoard.cardSuits.CLUBS, SchnapsenBoard.cardNames.TenOfClubs, 10));
+        unknownCards.add(new PlayingCard(SchnapsenBoard.cardSuits.CLUBS, SchnapsenBoard.cardNames.AceOfClubs, 11));
+
+        for(PlayingCard card :  unknownCards){
+            if(trumpSuit == card.getSuit()){
+                card.setIsTrumpSuit(true);
+            }
+        }
+        return unknownCards;
+    }
+
+    private boolean isRoundOver(Schnapsen schnapsen){
+        long oldBummerlValuePlayer0 = (long) oldUtilityPlayer0;
+        long oldBummerlValuePlayer1 = (long) oldUtilityPlayer1;
+        long newBummerlValuePlayer0 = (long) schnapsen.getUtilityValue(0);
+        long newBummerlValuePlayer1 =  (long) schnapsen.getUtilityValue(1);
+
+        if(oldBummerlValuePlayer0 != newBummerlValuePlayer0 || oldBummerlValuePlayer1 != newBummerlValuePlayer1)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+}
